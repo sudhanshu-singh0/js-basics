5df03cbb43693c845afb9f08633f4844fb734e99
sudhanshu-singh0 <146115208+sudhanshu-singh0@users.noreply.github.com>
2026-04-09 20:15:38 +0000

    dataTypes learnt

---
+++ js-basics/dataTypes.js
@@ -0,0 +1,7 @@
+let name="sudhanshu Singh";
+let age =25
+let height=170.34
+let ismale=true
+//bigInt
+//null 
+console.table([typeof name,typeof age,typeof height,typeof ismale,typeof null,typeof undefined])
--- js-basics/test.js
+++ js-basics/test.js
@@ -1,1 +1,11 @@
 console.log("hi there");
+console.log(1+2);
+const roll = 22365;
+let name="sudhanshu singh";
+let mail="sudhanshuu2003@gmail.com";
+let password="12345";
+without_any_thing = "hi there"  //here i have not used let/const/var and it is working
+
+
+
+console.table([roll,name,mail,password,without_any_thing]);
