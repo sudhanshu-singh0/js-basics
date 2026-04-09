6fb2a1cdcaa924f7906ea3a6a4f592401ca10c5b
sudhanshu-singh0 <146115208+sudhanshu-singh0@users.noreply.github.com>
2026-04-09 20:23:50 +0000

    conversion of datatypes

---
+++ js-basics/conversion.js
@@ -0,0 +1,10 @@
+let score=24
+let a=null
+let n="12221bj";
+let fal=0;
+let x=Number(score);
+let y=Number(a);
+let z=Number(n);
+let p=String(score)
+console.table([x,y,z,p,Boolean(score),Boolean(fal)])
+console.table([typeof x,typeof y,typeof z,typeof p])
